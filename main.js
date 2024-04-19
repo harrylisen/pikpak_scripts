9ad21d515010039d5db679a01540a557975175cb
harrylisen <46640121+harrylisen@users.noreply.github.com>
2024-04-19 14:32:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,19 +10,19 @@ async function main() {
   const password = response.data.password;
 
   console.log(username, password);
-  let pikPak = new PikPak(username, password, null, "49051761");
+  let pikPak = new PikPak(username, password, null, "95972652");
 
   pikPak
-    .authVerification()
-    .then(() => pikPak.getVerificationCode(mailjs))
-    .then(() => pikPak.authVerificationVerify())
-    .then(() => pikPak.authSignup())
-    .then(() => pikPak.userMe())
-    .then(() => pikPak.activityInvite())
-    .then(() => pikPak.activityInviteCode())
-    .then(() => pikPak.activationCode())
-    .then(() => pikPak.printUserInfo())
-    .catch((e) => console.log(e));
+    。authVerification()
+    。then(() => pikPak.getVerificationCode(mailjs))
+    。then(() => pikPak.authVerificationVerify())
+    。then(() => pikPak.authSignup())
+    。then(() => pikPak.userMe())
+    。then(() => pikPak.activityInvite())
+    。then(() => pikPak.activityInviteCode())
+    。then(() => pikPak.activationCode())
+    。then(() => pikPak.printUserInfo())
+    。catch((e) => console.log(e));
 
   // 登录
   // let pikPak = new PikPak('temp@temp.com', 'tempPassword', 'tempuuid');
